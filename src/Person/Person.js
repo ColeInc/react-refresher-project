de4180c80cbc0c47992701fadd97ce4d50a7ae48
ColeInc <chm@farmside.co.nz>
2019-03-09 14:03:35 +1300

    Initial project commit

---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>This is a Person Component: This person is called {props.name}, and they own a {props.pet}.</p> {/* can also mess around with this: {Math.round(Math.random()*10)} is {Math.round(Math.random()*100)} years old*/}
+            <p>Description of Pet: {props.children}</p>
+            <input type="input" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
+
+export default person;
