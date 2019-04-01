b34c805bb2ebcce0434b9ece3ef918e3e7a5c99f
ColeInc <chm@farmside.co.nz>
2019-04-02 11:57:23 +1300

    Updates

---
+++ precache-manifest.3dd427ac855507ca7d106771814f10ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13882f791fdf5a7a36bb",
+    "url": "/react-refresher-project/static/js/runtime~main.13882f79.js"
+  },
+  {
+    "revision": "7541c2d7c18dee0ac004",
+    "url": "/react-refresher-project/static/js/main.7541c2d7.chunk.js"
+  },
+  {
+    "revision": "0bbc8ec122111350b83a",
+    "url": "/react-refresher-project/static/js/2.0bbc8ec1.chunk.js"
+  },
+  {
+    "revision": "7541c2d7c18dee0ac004",
+    "url": "/react-refresher-project/static/css/main.9c027d65.chunk.css"
+  },
+  {
+    "revision": "b6477a72bf5614ee5877d33792afcba6",
+    "url": "/react-refresher-project/index.html"
+  }
+];
